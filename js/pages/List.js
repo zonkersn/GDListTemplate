b6c469b34c75df7ccd5c96e8178ecab1f3b767a3
zonkersn <157325224+zonkersn@users.noreply.github.com>
2024-06-01 14:23:34 -0400

    woah

--- js/pages/List.js
+++ js/pages/List.js
@@ -71,7 +71,7 @@ export default {
                                 <img v-if="record.mobile" :src="\`/assets/phone-landscape\${store.dark ? '-dark' : ''}.svg\`" alt="Mobile">
                             </td>
                             <td class="hz">
-                                <p>{{ record.hz }}Fps</p>
+                                <p>{{ record.hz }} FPS</p>
                             </td>
                         </tr>
                     </table>
